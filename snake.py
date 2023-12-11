36b19124e66438aa71132eab9c637c4906df596d
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2023-12-11 23:18:57 +0100

    does not totally work yet but almost there

--- snake.py
+++ snake.py
@@ -3,101 +3,135 @@ import random
 import argparse
 import logging
 import sys
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(sys.stderr)
-logger.addHandler(handler)
-parser = argparse.ArgumentParser(description='snake game')
-parser.add_argument('--bg-color-1', help="Permet de déterminer la 1ère couleur du damier."+
-                    "Valeur du type #hexadecimal")
-parser.add_argument('--bg-color-2', help="Permet de déterminer la 2ème couleur du damier."+
-                    "Valeur du type #hexadecimal")
-parser.add_argument('--height', help="Permet de déterminer la hauteur du damier."+
-                    "Valeur du type int")
-parser.add_argument('--width', help="Permet de déterminer la largeur du damier."+
-                    "Valeur du type int")
-parser.add_argument('--fps', help="Permet de déterminer le nombre d'actions par seconde."+
-                    "Valeur du type int")
-parser.add_argument('--fruit-color', help="Permet de déterminer la couleur du fruit."+
-                    "Valeur du type #hexadecimal")
-parser.add_argument('--snake-color', help="Permet de déterminer la couleur du serpent."+
-                    "Valeur du type #hexadecimal")
-parser.add_argument('--snake-length', help="Permet de déterminer la longueur initiale du serpent."+
-                    "Valeur du type int")
-parser.add_argument('--tile-size', help="Permet de déterminer la taille d'un carreau du damier."+
-                    "Valeur du type int")
-parser.add_argument('--gameover-on-exit', help='flag : activer pour mourir quand on sort', action='store_true')
-parser.add_argument('--debug', help='flag : activer pour messages de debug', action='store_true')
-args = parser.parse_args()
 
-#création des constantes
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 GREEN=(0,255,0)
 RED=(255,0,0)
-WIDTH=int(args.width)
-HEIGHT=int(args.height)
-TIME=int(args.fps)
-LARGEUR=int(args.tile_size)
-LONGUEUR=int(args.snake_length)
-POS1=[10*LARGEUR,7*LARGEUR]
-POS2=[10*LARGEUR,8*LARGEUR]
-POS3=[10*LARGEUR,9*LARGEUR]
 
-#vérification des arguments
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stderr)
+logger.addHandler(handler)
+parser = argparse.ArgumentParser(description='snake game')
 
-#tile_size divise height
-if HEIGHT % LARGEUR !=0 :
-    raise ValueError("Height must be a multiple of tile size")
-#tile_size divise width
-if WIDTH % LARGEUR !=0 :
-    raise ValueError("Width must be a multiple of tile size")
-#20 colonnes minimum
-MIN_COLUMNS = 20
-if  WIDTH//LARGEUR < MIN_COLUMNS :
-    raise ValueError("There must be at least %d columns" % MIN_COLUMNS)
-#12 lignes minimum
-MIN_ROWS = 12
-if HEIGHT//LARGEUR < MIN_ROWS :
-    raise ValueError("There must be at least %d rows" % MIN_ROWS)
-#longueur initiale du serpent plus grande que 2
-MIN_SIZE=2
-if LONGUEUR < MIN_SIZE:
-    raise ValueError("The initial size of the snake must be greater or equal to %d." % MIN_SIZE)
-#couleur du serpent et du damier différentes
-if args.snake_color == args.bg_color_1 or args.snake_color == args.bg_color_2 :
-    raise ValueError("Snake color must be different from checkboard color")
+def read_args():
+    parser.add_argument('--bg-color-1', help="Permet de déterminer la 1ère couleur du damier."+
+                        "Valeur du type #hexadecimal")
+    parser.add_argument('--bg-color-2', help="Permet de déterminer la 2ème couleur du damier."+
+                        "Valeur du type #hexadecimal")
+    parser.add_argument('--height', help="Permet de déterminer la hauteur du damier."+
+                        "Valeur du type int")
+    parser.add_argument('--width', help="Permet de déterminer la largeur du damier."+
+                        "Valeur du type int")
+    parser.add_argument('--fps', help="Permet de déterminer le nombre d'actions par seconde."+
+                        "Valeur du type int")
+    parser.add_argument('--fruit-color', help="Permet de déterminer la couleur du fruit."+
+                        "Valeur du type #hexadecimal")
+    parser.add_argument('--snake-color', help="Permet de déterminer la couleur du serpent."+
+                        "Valeur du type #hexadecimal")
+    parser.add_argument('--snake-length', help="Permet de déterminer la longueur initiale du serpent."+
+                        "Valeur du type int")
+    parser.add_argument('--tile-size', help="Permet de déterminer la taille d'un carreau du damier."+
+                        "Valeur du type int")
+    parser.add_argument('--gameover-on-exit', help='flag : activer pour mourir quand on sort', action='store_true')
+    parser.add_argument('--debug', help='flag : activer pour messages de debug', action='store_true')
+    args = parser.parse_args()
 
-#initialisation des variables
-longueur=LONGUEUR
-execute=True
-direction=(0,1)
-mange=False
-score=0
-curseur=0
-serpent = [POS3,POS2,POS1]
+    #création des constantes
+    width=int(args.width)
+    height=int(args.height)
+    time=int(args.fps)
+    largeur=int(args.tile_size)
+    longueur=int(args.snake_length)
+    color1 = args.bg_color_1
+    color2 = args.bg_color_2
+    couleur = args.snake_color 
+    fruit_color = args.fruit_color
+    game_over_on_exit = args.gameover_on_exit
+    debug = args.debug
 
-#coordonnées de la pomme générées aléatoirement
-pomme1=random.randrange(0,HEIGHT,LARGEUR)
-pomme2=random.randrange(0,WIDTH,LARGEUR)
 
-#afficher messages de debug s'il faut
-if args.debug :
-    logger.setLevel(logging.DEBUG)
+    #vérification des arguments
 
-pygame.init()
-screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
-clock = pygame.time.Clock()
+    #tile_size divise height
+    if height % largeur !=0 :
+        raise ValueError("Height must be a multiple of tile size")
+    #tile_size divise width
+    if width % largeur !=0 :
+        raise ValueError("Width must be a multiple of tile size")
+    #20 colonnes minimum
+    MIN_COLUMNS = 20
+    if  width//largeur < MIN_COLUMNS :
+        raise ValueError("There must be at least %d columns" % MIN_COLUMNS)
+    #12 lignes minimum
+    MIN_ROWS = 12
+    if height//largeur < MIN_ROWS :
+        raise ValueError("There must be at least %d rows" % MIN_ROWS)
+    #longueur initiale du serpent plus grande que 2
+    MIN_SIZE=2
+    if longueur < MIN_SIZE:
+        raise ValueError("The initial size of the snake must be greater or equal to %d." % MIN_SIZE)
+    #couleur du serpent et du damier différentes
+    if args.snake_color == args.bg_color_1 or args.snake_color == args.bg_color_2 :
+        raise ValueError("Snake color must be different from checkboard color")
+    
+    #afficher messages de debug s'il faut
+    if args.debug :
+        logger.setLevel(logging.DEBUG)
+    
+    return (width, height, time, largeur, longueur, color1, color2, couleur, fruit_color, game_over_on_exit, debug)
 
-logger.debug('Start main loop')
+def draw_checkerboard(screen, COLOR1, COLOR2, WIDTH, LARGEUR, HEIGHT):
+    screen.fill( COLOR2 )
+    
+    left=0
+    #on dessine une rangée sur 2
+    while left<WIDTH:
+        top=0
+        while top<HEIGHT:
+            rect = pygame.Rect(left, top, LARGEUR, LARGEUR)
+            pygame.draw.rect(screen, COLOR1, rect)
+            top+=2*LARGEUR
+        left+=2*LARGEUR
+    #puis le reste des rangées en décalant d'une colonne
+    left=LARGEUR
+    while left<WIDTH:
+        top=LARGEUR
+        while top<HEIGHT:
+            rect = pygame.Rect(left, top, LARGEUR, LARGEUR)
+            pygame.draw.rect(screen, COLOR1, rect)
+            top+=2*LARGEUR
+        left+=2*LARGEUR
 
-while execute==True:
+def get_score(longueur) :
+    score=(longueur-3)*10
+    return score
 
-    clock.tick(TIME)
+def draw_fruit(screen, FRUIT_COLOR, pomme1, pomme2, LARGEUR, longueur):
+    rect = pygame.Rect(pomme1, pomme2, LARGEUR, LARGEUR)
+    pygame.draw.rect(screen, FRUIT_COLOR , rect)
 
-    #faire avancer le serpent tout seul
-    serpent=[[serpent[0][0]+LARGEUR*direction[0],serpent[0][1]+LARGEUR*direction[1]]]+serpent
-    serpent.pop()
+def draw_snake(screen,COULEUR, LARGEUR, serpent, longueur):
+    curseur = 0
+    while curseur<longueur:
+        pos=serpent[curseur]
+        rect = pygame.Rect(pos[1], pos[0], LARGEUR, LARGEUR)
+        pygame.draw.rect(screen, COULEUR, rect)
+        curseur+=1
+    curseur=0
 
+def draw(screen, FRUIT_COLOR, COULEUR, COLOR1, COLOR2, HEIGHT, LARGEUR, WIDTH, pomme1, pomme2, serpent, longueur):
+    draw_checkerboard(screen, COLOR1, COLOR2, WIDTH, LARGEUR, HEIGHT)
+    draw_fruit(screen, FRUIT_COLOR, pomme1, pomme2, LARGEUR, longueur)
+    draw_snake(screen, COULEUR, LARGEUR, serpent, longueur)
+
+def update_display(screen, FRUIT_COLOR, COULEUR, COLOR1, COLOR2, HEIGHT, LARGEUR, WIDTH, pomme1, pomme2, serpent, longueur):
+    draw(screen, FRUIT_COLOR, COULEUR, COLOR1, COLOR2, HEIGHT, LARGEUR, WIDTH, pomme1, pomme2, serpent, longueur)
+    score= get_score(longueur)
+    pygame.display.set_caption("SCORE :"+str(score))
+    pygame.display.update()
+
+def process_events(LARGEUR, GAME_OVER_ON_EXIT, longueur, HEIGHT, WIDTH, pomme1, pomme2, execute, serpent, mange, direction):
     for event in pygame.event.get():
 
         #pouvoir fermer la fenêtre
@@ -138,7 +172,7 @@ while execute==True:
                     serpent=[[serpent[0][0]-LARGEUR,serpent[0][1]]]+serpent
                     serpent.pop()
 
-    if args.gameover_on_exit :
+    if GAME_OVER_ON_EXIT :
         #vérifier que le serpent ne sort pas du cadre
         if serpent[0][0]<0 and direction[0]==-1:
             execute=False
@@ -167,69 +201,70 @@ while execute==True:
         if serpent[0][1]>WIDTH and direction[1]==1:
             for i in range(longueur):
                 serpent[i][1]=serpent[i][1]-WIDTH
-
-    #dessiner le damier :
-    color2=args.bg_color_2
-    screen.fill( color2 )
-    color1 = args.bg_color_1
-    left=0
-    #on dessine une rangée sur 2
-    while left<WIDTH:
-        top=0
-        while top<HEIGHT:
-            rect = pygame.Rect(left, top, LARGEUR, LARGEUR)
-            pygame.draw.rect(screen, color1, rect)
-            top+=2*LARGEUR
-        left+=2*LARGEUR
-    #puis le reste des rangées en décalant d'une colonne
-    left=LARGEUR
-    while left<WIDTH:
-        top=LARGEUR
-        while top<HEIGHT:
-            rect = pygame.Rect(left, top, LARGEUR, LARGEUR)
-            pygame.draw.rect(screen, color1, rect)
-            top+=2*LARGEUR
-        left+=2*LARGEUR
-    
-
-    #dessiner le serpent
-    couleur=args.snake_color
-    while curseur<longueur:
-        pos=serpent[curseur]
-        rect = pygame.Rect(pos[1], pos[0], LARGEUR, LARGEUR)
-        pygame.draw.rect(screen, couleur, rect)
-        curseur+=1
-    curseur=0
-
-    rect = pygame.Rect(pomme2, pomme1, LARGEUR, LARGEUR)
-    pygame.draw.rect(screen, RED , rect)
     
     #détecte quand le serpent mange la pomme 
     if serpent[0]==[pomme1,pomme2]:
         mange=True
-    
-    #génère une nouvelle pomme
-    if mange:
-        couleur_pomme=args.fruit_color
+
+    if mange :  
+        (pomme1, pomme2) = update_fruit(HEIGHT, LARGEUR, WIDTH)
         serpent=[[pomme1,pomme2]]+serpent
         longueur+=1
-        pomme1=random.randrange(0,HEIGHT,LARGEUR)
-        pomme2=random.randrange(0,WIDTH,LARGEUR)
-        rect = pygame.Rect(pomme1, pomme2, LARGEUR, LARGEUR)
-        pygame.draw.rect(screen, couleur_pomme , rect)
         logger.debug('Snake has eaten a fruit')
         mange=False
-        
-    #affiche le score
-    score=(longueur-3)*10
-    pygame.display.set_caption("SCORE :"+str(score))
-    pygame.display.update()
 
     #gérer la collision
     for e in serpent[1:] : 
         if serpent [0]==e :
             execute = False
             logger.info('The snake collided itself')
+    
+    return (serpent, direction, execute, mange, pomme1, pomme2)
 
-pygame.quit()
-logger.debug('Game over')
+def move_snake(serpent,LARGEUR, direction):
+    serpent=[[serpent[0][0]+LARGEUR*direction[0],serpent[0][1]+LARGEUR*direction[1]]]+serpent
+    serpent.pop()
+    return (serpent)
+
+def update_fruit(HEIGHT, LARGEUR, WIDTH):
+    pomme1=random.randrange(0,HEIGHT,LARGEUR)
+    pomme2=random.randrange(0,WIDTH,LARGEUR)
+    return (pomme1, pomme2)
+
+def main():
+
+    #initialisation des variables
+    longueur=LONGUEUR
+    execute=True
+    direction=(0,1)
+    mange=False
+    score=0
+    curseur=0
+    POS1=[10*LARGEUR,7*LARGEUR]
+    POS2=[10*LARGEUR,8*LARGEUR]
+    POS3=[10*LARGEUR,9*LARGEUR]
+    serpent = [POS3,POS2,POS1]
+    (pomme1 , pomme2) =update_fruit(HEIGHT, LARGEUR, WIDTH)
+
+    pygame.init()
+    screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+    clock = pygame.time.Clock()
+
+    logger.debug('Start main loop')
+
+    print(execute)
+
+    while execute==True:
+
+        clock.tick(TIME)
+        serpent = move_snake(serpent,LARGEUR, direction)
+        print(serpent)
+        (serpent, direction, execute, mange, pomme1, pomme2) = process_events(LARGEUR, GAME_OVER_ON_EXIT, longueur, HEIGHT, WIDTH, pomme1, pomme2, execute, serpent, mange, direction)
+        update_display(screen, FRUIT_COLOR, COULEUR, COLOR1, COLOR2, HEIGHT, LARGEUR, WIDTH, pomme1, pomme2, serpent, longueur)
+
+    pygame.quit()
+    logger.debug('Game over')
+
+(WIDTH, HEIGHT, TIME, LARGEUR, LONGUEUR, COLOR1, COLOR2, COULEUR, FRUIT_COLOR, GAME_OVER_ON_EXIT, DEBUG) = read_args()
+
+main()
