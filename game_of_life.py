57f2161426c21b6084746173fc8f9c659524e85f
ElisaSko <144114343+ElisaSko@users.noreply.github.com>
2023-12-28 14:54:06 +0100

    35 manage the evolution of the cells (#36)
    * management of the cells in the cell class
    
    * creation of an update function in set_of_cells, that manages the initialization and the output too
    
    * new variable to track the number of iterations

--- game_of_life.py
+++ game_of_life.py
@@ -13,12 +13,39 @@ class Cell :
         self.x = x
         self.y = y
         self.alive = alive
+    
+    def neighbours(self, set_of_cells):
+        neighbours=[]
+        for x in range(self.x-1, self.x+2):
+            for y in range(self.y-1, self.y+2):
+                if x >= 0 and x < set_of_cells.height and y >= 0 and y < set_of_cells.width and (x != self.x or y != self.y):
+                    neighbours.append(set_of_cells.cells[x][y])
 
-class Set_of_cells  :
-    def __init__(self, cells, height, width):
+    def count_neighbours(self, set_of_cells):
+        neighbours = self.neighbours(set_of_cells)
+        count = 0
+        for cell in neighbours:
+            if cell.alive:
+                count += 1
+        return count
+    
+    def update (self, set_of_cells):
+        number = self.count_neighbours(set_of_cells)
+        if self.alive:
+            if number = 2 or number = 3:
+                self.alive = True
+            else :
+                self.alive = False
+        else:
+            if number == 3:
+                self.alive = True
+
+class Set_Of_Cells  :
+    def __init__(self, cells, height, width, iteration):
         self.cells = cells
         self.height = height
         self.width = width
+        self.iteration = iteration
     
     def initialize(self, height, width, pattern):
         self.height = height
@@ -31,6 +58,17 @@ class Set_of_cells  :
             self.cells.append(line)
         for cell in pattern.cells:
             self.cells[cell.x][cell.y].alive = cell.alive
+        
+    def update(self, pattern, height, width, max_iteration, output_file, input_file):
+        if self.iteration == 0:
+            self.initialize(self, height, width, pattern)
+        if self.iteration < max_iteration:
+            for x in range(self.height):
+                for y in range(self.width):
+                    self.cells[x][y].update(self)
+            self.iteration += 1
+        elif self.iteration == max_iteration:
+            self.output(self,output_file)
     
     def output(self, file_name):
         file = open(file_name, 'w')
